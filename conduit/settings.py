1fbf9ca51a0b21ac84fa0ef54bfc631d4fd75820
tchawou-daniel <daniel-jordan.sipe-tchawou@lacatholille.fr>
2021-01-17 20:12:17 +0100

    :wrench: update Procfile and settings files

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,6 +25,8 @@ class Config(object):
         'http://localhost:4200',
         'http://0.0.0.0:4000',
         'http://localhost:4000',
+        'http://0.0.0.0:8080',
+        'https://0.0.0.0:8080',
         'https://2020devopsfinalfront.netlify.app',
     ]
     JWT_HEADER_TYPE = 'Token'
